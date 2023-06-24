cd75153e9f7e30eede36e3b6abc518e001b3e11d
Juan <juanbarciaq@gmail.com>
2023-06-24 13:48:07 -0500

    commit inicial

---
+++ sad/deportistas/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from deportistas.models import Deportista, Sede, Competencia
+# Register your models here.
+admin.site.register(Deportista)
+admin.site.register(Sede)
+admin.site.register(Competencia)
